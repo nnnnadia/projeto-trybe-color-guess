6fa52ee441313f2431000f960ec287b7ecbbb42c
Nádia <tristaonadia@gmail.com>
2022-02-19 22:51:17 -0300

    Requisito 3 e 4 feitos.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const writtenColor = document.querySelector('#rgb-color');
+const posibleColors = document.querySelector('#posible-colors');
 
 /*
   getRandomColor recebe três valores aleatórios entre 0 e 255 de getRandomBtn0255 e retorna uma cor no formato rgb(x , y , z).
 */
 
 function getRandomBtn0255() {
-  const colorValue = Math.ceil(Math.random() * 255);
+  const colorValue = Math.round(Math.random() * 255);
   return colorValue;
 }
 
@@ -18,10 +19,40 @@ function getRandomColor() {
 }
 
 function generateColorToGuess() {
-  const secretColor = getRandomColor();
-  writtenColor.innerText = secretColor;
+  const mysteryColor = getRandomColor();
+  writtenColor.innerText = mysteryColor;
+  return mysteryColor;
+}
+
+function generateColorList(mysteryColor) {
+  const mysteryPosition = Math.round(Math.random() * 5);
+  const colorList = [];
+  for (let i = 0; i < 6; i += 1) {
+    if (i !== mysteryPosition) {
+      colorList.push(getRandomColor());
+    } else {
+      colorList.push(mysteryColor);
+    }
+  }
+  return colorList;
+}
+
+function appendPosibleColors(colorList) {
+  for (let color of colorList) {
+    const colorPicker = document.createElement('div');
+    const colorPickerColor = getRandomColor();
+    colorPicker.style.backgroundColor = colorPickerColor;
+    colorPicker.classList.add('ball');
+    posibleColors.appendChild(colorPicker);
+  }
+}
+
+function setGame() {
+  const mysteryColor = generateColorToGuess();
+  const colorList = generateColorList(mysteryColor);
+  appendPosibleColors(colorList);
 }
 
 window.onload = () => {
-  generateColorToGuess();
+  setGame();
 };
