dacd9735e2fd8c09dc33fa1938ab3581b7d848d5
Nádia <tristaonadia@gmail.com>
2022-02-20 11:01:55 -0300

    Requisito 5 feito.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const writtenColor = document.querySelector('#rgb-color');
 const posibleColors = document.querySelector('#posible-colors');
+const commandText = document.querySelector('#answer');
 
 /*
   getRandomColor recebe três valores aleatórios entre 0 e 255 de getRandomBtn0255 e retorna uma cor no formato rgb(x , y , z).
@@ -14,7 +15,7 @@ function getRandomColor() {
   const r = getRandomBtn0255();
   const g = getRandomBtn0255();
   const b = getRandomBtn0255();
-  const colorRGB = `rgb(${r} , ${g}, ${b})`;
+  const colorRGB = `rgb(${r}, ${g}, ${b})`;
   return colorRGB;
 }
 
@@ -24,25 +25,53 @@ function generateColorToGuess() {
   return mysteryColor;
 }
 
+/*
+  colorList = [
+    {
+      color: rgb (x , y , z),
+      answer: false,
+    },
+    {
+      color: rgb (x , y , z),
+      answer: true,
+    },
+  ]
+*/
 function generateColorList(mysteryColor) {
   const mysteryPosition = Math.round(Math.random() * 5);
   const colorList = [];
   for (let i = 0; i < 6; i += 1) {
+    const colorObject = {};
     if (i !== mysteryPosition) {
-      colorList.push(getRandomColor());
+      colorObject.color = getRandomColor();
+      colorObject.answer = false;
     } else {
-      colorList.push(mysteryColor);
+      colorObject.color = mysteryColor;
+      colorObject.answer = true;
     }
+    colorList.push(colorObject);
   }
   return colorList;
 }
 
+function checkAnswer(event) {
+  if (event.target.classList.contains('answer')) {
+    commandText.innerText = 'Acertou!';
+  } else {
+    commandText.innerText = 'Errou! Tente novamente!';
+  }
+}
+
 function appendPosibleColors(colorList) {
-  for (let color of colorList) {
+  for (let i = 0; i < 6; i += 1) {
     const colorPicker = document.createElement('div');
-    const colorPickerColor = getRandomColor();
+    const colorPickerColor = colorList[i].color;
     colorPicker.style.backgroundColor = colorPickerColor;
     colorPicker.classList.add('ball');
+    if (colorList[i].answer === true) {
+      colorPicker.classList.add('answer');
+    }
+    colorPicker.addEventListener('click', checkAnswer);
     posibleColors.appendChild(colorPicker);
   }
 }
